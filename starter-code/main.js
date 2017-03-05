9a0c06c12eab46e61c91897a0c3a03b36cbb80b1
514700 <birenjaspal@C1MRQ144HF4H.local>
2017-03-05 11:56:24 -0500

    added card comparison check

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,19 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardThree !== cardFour) {
+  alert('Sorry, try again.');
+}
+else {
+  alert('You found a match!');
+  if (cardTwo === "king") {
+    alert('Both cards are kings!');
+  }
+  else if (cardTwo === "queen") {
+    alert('Both cards are queens!');
+  }
+  else {
+    alert('error');
+  }
+}
