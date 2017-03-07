700df7c7154fe93b35646c3fbc841f69ecd85e58
514700 <birenjaspal@C1MRQ144HF4H.local>
2017-03-06 22:26:12 -0500

    memory game complete

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,32 +1,51 @@
-console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-/* if (cardThree !== cardFour) {
-  alert('Sorry, try again.');
-}
-else {
-  alert('You found a match!');
-  if (cardTwo === "king") {
-    alert('Both cards are kings!');
-  }
-  else if (cardTwo === "queen") {
-    alert('Both cards are queens!');
-  }
-  else {
-    alert('error');
-  }
-} */
-document.getElementById('game-board');
+//console.log("JS file is connected to HTML! Woo!")
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+//Get an element with the id of game-board and set it to a variable.
+var memoryBoard = document.getElementById('game-board');
 
 var createCards = function(){
-  for (x=0; x<4; x+=1){
-    var cards=document.createElement('div');
-    cards.className = 'card';
-    game-board.appendChild(cards);
+  for (var i=0; i<cards.length; i++){
+    // creates an HTML element for each card. each HTML element should be a div and have the class card
+    var cardElement=document.createElement('div');
+    cardElement.addEventListener('click', isTwoCards);
+    //adds a class... <div class='card'></div>
+    cardElement.className = 'card';
+    cardElement.setAttribute('card-data', cards[i]);
+    memoryBoard.appendChild(cardElement);
   }
+}
+
+var isMatch = function(cardsInPlay){
+  if (cardsInPlay[0] !== cardsInPlay[1]) {
+    alert('Sorry, try again.');
+  }
+  else {
+    alert('You found a match!');
+    if (cardsInPlay[0] === "king") {
+      alert('Both cards are kings!');
+    }
+    else if (cardsInPlay[0] === "queen") {
+      alert('Both cards are queens!');
+    }
+    else {
+      alert('error');
+    }
+  }
+}
+
+var isTwoCards = function(){
+  cardsInPlay.push(this.getAttribute('card-data'));
+  if (this.getAttribute('card-data') === "king"){
+    this.innerHTML = '<img src="king.png" alt="King of Spades" />'
+  }
+  else {
+    this.innerHTML = '<img src="queen.png" alt="Queen of Clubs" />'
+  }
+  if (cardsInPlay.length === 2) {
+      isMatch(cardsInPlay); 
+       cardsInPlay = [];
+     }
 }
 
 createCards();
